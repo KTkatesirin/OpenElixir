e409fbca521339150d1eebd7b3667a66af4e05fc
KTkatesirin <120615723+KTkatesirin@users.noreply.github.com>
2023-07-19 03:53:36 +0700

    demo_app

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Initial setup
+
+mix deps.get --only prod
+
+MIX_ENV=prod mix compile
+
+# Compile assets
+
+MIX_ENV=prod mix assets.deploy
+mix phx.gen.release
+mix phx.digest
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
+
+# Run migrations
+_build/prod/rel/demo_app/bin/demo_app eval "DemoApp.Release.migrate"
